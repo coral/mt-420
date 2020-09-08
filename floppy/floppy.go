788463110c1f9cf3e5cbfb1023edea6b0040398f
coral <jonas@jonasbengtson.se>
2020-09-07 20:46:00 -0700

    Ye

--- floppy/floppy.go
+++ floppy/floppy.go
@@ -2,21 +2,47 @@ package floppy
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"os/exec"
 )
 
 type Disk struct {
 	device     string
 	mountpoint string
+	fileindex  []os.FileInfo
 }
 
-func New(device string) *Disk {
+func New(device string, mountpoint string) *Disk {
 	return &Disk{
-		device: device,
+		device:     device,
+		mountpoint: mountpoint,
 	}
 }
 
 func (d *Disk) Watch() {
 	cmd := exec.Command("diskd", "-d", d.device).Run()
 	fmt.Println(cmd)
+	d.mount()
+}
+
+func (d *Disk) mount() {
+	cmd := exec.Command("mount", d.mountpoint).Run()
+	fmt.Println(cmd)
+
+	d.indexDisk()
+}
+
+func (d *Disk) indexDisk() {
+	files, err := ioutil.ReadDir(d.mountpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d.fileindex = files
+
+	for _, file := range d.fileindex {
+		fmt.Println(file.Name())
+	}
 }
